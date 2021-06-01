49ef81ddf112b54a944866d186c9c9405e66d72f
yijie4188 <yijie4188@gmail.com>
2021-06-01 14:27:49 +0800

    feat(main): base function
    * show hostname
    * run command
    * judge command is run right

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <unistd.h>
+#include "core/consoleColor.h"
 
 using namespace std;
 
 int main() {
+    bool isRight = true;
+    char hostname[1024];
+    gethostname(hostname, 1024);
+
     string input;
     while (true) {
-        cout << "username@host-name >> ";
+        cout << (isRight ? "" : RED) << "username@" << hostname << " > " << RESET;
         getline(cin, input);
-        cout << input << endl;
         if (input == "exit") { break; }
+        if (!input.empty()) {
+            isRight = system(
+                ("./bin/" + input).c_str()
+            ) == 0;
+        }
     }
     return 0;
 }
