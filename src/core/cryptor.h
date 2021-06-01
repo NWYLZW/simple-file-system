a4f828e29aa5cd98693ee405436aed4c2746fad4
yijie4188 <yijie4188@gmail.com>
2021-06-01 22:37:59 +0800

    feat(core): add virginia algorithm

--- src/core/cryptor.h
+++ src/core/cryptor.h
@@ -1,6 +1,21 @@
 #pragma once
+#include <stdlib.h>
+#include <string.h>
 
 class Cryptor {
 public:
-    int zero() { return 0; }
+    static char* virginia(char* text, char* key, int isDe = false) {
+        int i, j = 0, z = 0;
+        int textLen = strlen(text), keyLen = strlen(key);
+        char* result = (char *)malloc((textLen + 1) * sizeof(char));
+        for (i = 0;i < textLen; i++) {
+            if (isDe) {
+                result[z] = (text[i] - key[j%keyLen] + 26) % 26 + 'a';
+            } else {
+                result[z] = (text[i] - 'a' + key[j%keyLen] - 'a') % 26 + 'a';
+            }
+            j++, z++;
+        }
+        return result;
+    }
 };
