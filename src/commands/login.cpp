d9ea0b9de382815eb51f29a90dd0824fd123a136
yijie4188 <yijie4188@gmail.com>
2021-06-01 15:34:58 +0800

    feat(cmd): login help message

--- src/commands/login.cpp
+++ src/commands/login.cpp
@@ -1,8 +1,31 @@
 #include "login.h"
 
+static std::vector<const char *> HELP_MSGS = {
+    "login <username> [password]",
+    "Usage login",
+    "",
+    "  -h, --help    get help message.",
+    "",
+    "login user."
+};
+
 int main (
     int argc, char* *args
 ) {
-    std::cout << "this is login." << std::endl;
+    if (argc <= 1) {
+        std::cout << "Params is empty." << std::endl;
+        return 1;
+    }
+    for (int i = 0;i < argc; i++) {
+        std::string arg = args[i];
+        if (
+            arg == "-h" || arg == "--help"
+        ) {
+            for (const auto &HELP_MSG : HELP_MSGS) {
+                std::cout << HELP_MSG << std::endl;
+            }
+            return 0;
+        }
+    }
     return 0;
 }
