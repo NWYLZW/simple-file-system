35673419475f378614b300165a91ec2ba8fcabca
yijie4188 <yijie4188@gmail.com>
2021-06-01 15:09:41 +0800

    feat(main): get username and fix null line will change to red

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
+#include <pwd.h>
 #include <iostream>
 #include <unistd.h>
+
 #include "core/consoleColor.h"
 
 using namespace std;
+
+const char * curUsername() {
+    struct passwd *pwd = getpwuid(getuid());
+    return pwd ? pwd->pw_name : "(?)";
+}
 
 int main() {
     bool isRight = true;
@@ -11,7 +18,7 @@ int main() {
 
     string input;
     while (true) {
-        cout << (isRight ? "" : RED) << "username@" << hostname << " > " << RESET;
+        cout << (isRight ? "" : RED) << curUsername() << "@[" << hostname << "]> " << RESET;
         getline(cin, input);
         if (input == "exit") {
             cout << "See you~" << endl;
@@ -21,6 +28,8 @@ int main() {
             isRight = system(
                 ("./bin/" + input).c_str()
             ) == 0;
+        } else {
+            isRight = true;
         }
     }
     return 0;
