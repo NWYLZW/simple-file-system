4324995887f134d4a50d752e46a18e0498438e50
yijie4188 <yijie4188@gmail.com>
2021-06-02 10:45:07 +0800

    feat(core): simple crypto

--- src/core/cryptor.h
+++ src/core/cryptor.h
@@ -1,20 +1,14 @@
 #pragma once
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
 
 class Cryptor {
 public:
-    static char* virginia(char* text, char* key, int isDe = false) {
-        int i, j = 0, z = 0;
-        int textLen = strlen(text), keyLen = strlen(key);
-        char* result = (char *)malloc((textLen + 1) * sizeof(char));
-        for (i = 0;i < textLen; i++) {
-            if (isDe) {
-                result[z] = (text[i] - key[j%keyLen] + 26) % 26 + 'a';
-            } else {
-                result[z] = (text[i] - 'a' + key[j%keyLen] - 'a') % 26 + 'a';
-            }
-            j++, z++;
+    static string simple(
+        string text, string key
+    ) {
+        string result = "";
+        for (int i = 0;i < text.length(); i++) {
+            result += text[i] ^ key[i % key.length()];
         }
         return result;
     }
