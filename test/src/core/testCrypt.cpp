7e484a154fdae492fb1707fd7cdae8082df593c1
yijie4188 <yijie4188@gmail.com>
2021-06-02 10:45:28 +0800

    feat(test): unit test simple crypto

--- test/src/core/testCrypt.cpp
+++ test/src/core/testCrypt.cpp
@@ -1,20 +1,23 @@
+#include "iostream"
+using namespace std;
+
 #include "gtest/gtest.h"
 
 #include "core/cryptor.h"
 
 namespace {
     class TestClass:public testing::Test{
-    public:
-        Cryptor testCryptor;
     };
 }
 
 TEST_F(TestClass, Test_ReturnZeroMethod){
-    char* plainText = "some text";
-    char* ciphertext = testCryptor.virginia(plainText, "123456");
-    ASSERT_EQ(
-        testCryptor.virginia(
-            ciphertext, "123456", true
+    string plainText  = (string) "some text";
+    string cipherText = (string) Cryptor::simple(
+        plainText.c_str(), "123456"
+    );
+    GTEST_ASSERT_EQ(
+        (string) Cryptor::simple(
+            cipherText.c_str(), "123456"
         ), plainText
     );
 }
